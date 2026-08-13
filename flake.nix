ecb1ac511cb46b21a50f55f86a5b34384f07be54
John McBride <jpmmcbride@gmail.com>
2026-08-13 16:04:28 -0500

    chore: manage Dagger via Nix flake (0.21.8)

--- flake.nix
+++ flake.nix
@@ -4,11 +4,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-skills.url = "github:papercomputeco/flake-skills";
+    dagger.url = "github:dagger/nix";
+    dagger.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-skills }:
-    flake-skills.lib.mkSkillsFlake {
-      inherit nixpkgs;
-      skillsSrc = ./skills;
+  outputs = { self, nixpkgs, flake-skills, dagger }:
+    let
+      skillsFlake = flake-skills.lib.mkSkillsFlake {
+        inherit nixpkgs;
+        skillsSrc = ./skills;
+      };
+    in
+    skillsFlake // {
+      devShells = nixpkgs.lib.mapAttrs (system: shells: shells // {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          name = "skills-dev";
+          buildInputs = [
+            dagger.packages.${system}.dagger
+          ];
+          shellHook = skillsFlake.lib.mkSkillsHook {
+            skills = skillsFlake.skillNames;
+          };
+        };
+      }) skillsFlake.devShells;
     };
 }
